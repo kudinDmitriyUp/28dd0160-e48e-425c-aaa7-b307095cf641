d1d5103c6270f4aa50fa4a587ea3e43026e8c169
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-03-26 17:09:17 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Source_Sans_3 } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Lumina Skincare | Organic & Sustainable Beauty',
+  description: 'Discover clean, organic skincare that nourishes your skin and spirit. Handcrafted beauty products designed for natural radiance.',
+  openGraph: {
+    "siteName": "Lumina Skincare",
+    "type": "website"
+  },
+};
+
+const sourceSans3 = Source_Sans_3({
+  variable: "--font-source-sans-3",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${sourceSans3.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
